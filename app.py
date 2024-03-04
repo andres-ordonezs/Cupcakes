70079ed91d7e359ec92992d53bc7f562b6a8a771
andres-ordonezs <andresmw.89@gmail.com>
2024-03-04 14:49:52 -0500

    First commit

---
+++ app.py
@@ -0,0 +1,11 @@
+"""Flask app for Cupcakes"""
+import os
+from flask import Flask, request, jsonify
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", 'postgresql:///cupcakes')
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
